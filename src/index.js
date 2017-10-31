b04fd818d89747e88e681de45f8deebf39f69bd0
wenche <wenche.tollevsen@gmail.com>
2017-10-31 12:29:54 +0100

    feat: add global events

--- src/index.js
+++ src/index.js
@@ -1,40 +1,83 @@
+/* eslint no-underscore-dangle: off */
 const eventListeners = {};
 
-const listenTo = (eventName, func) => {
-  if (typeof func !== 'function') {
-    /* eslint no-console: off */
-    console.error(`Listener to ${eventName}  is not a function`);
+if (!global._nocmsEventListeners) {
+  global._nocmsEventListeners = {};
+}
+
+const _trigger = (eventName, eventStore, ...args) => {
+  if (!eventStore[eventName]) {
     return;
   }
-  if (!eventListeners[eventName]) {
-    eventListeners[eventName] = [];
-  }
-  eventListeners[eventName].push(func);
-};
-
-const stopListenTo = (eventName, func) => {
-  const index = eventListeners[eventName].indexOf(func);
-  if (index !== -1) {
-    eventListeners[eventName].splice(index, 1);
-  }
-};
-
-const trigger = (eventName, ...args) => {
-  if (!eventListeners[eventName]) {
-    return;
-  }
-  eventListeners[eventName].forEach((f) => {
+  eventStore[eventName].forEach((f) => {
     f.apply(this, args);
   });
 };
 
-const clearEvent = (eventName) => {
-  delete eventListeners[eventName];
+const _listenTo = (eventName, func, eventStore) => {
+  if (typeof func !== 'function') {
+    /* eslint no-console: off */
+    /* eslint no-param-reassign: off  */
+    console.error(`Listener to ${eventName}  is not a function`);
+    return;
+  }
+  if (!eventStore[eventName]) {
+    eventStore[eventName] = [];
+  }
+  eventStore[eventName].push(func);
 };
+
+const _stopListenTo = (eventName, func, eventStore) => {
+  const index = eventStore[eventName].indexOf(func);
+  if (index !== -1) {
+    eventStore[eventName].splice(index, 1);
+  }
+};
+
+const _clearEvent = (eventName, eventStore = eventListeners) => {
+  delete eventStore[eventName];
+};
+
+const trigger = (eventName, ...args) => {
+  _trigger(eventName, eventListeners, ...args);
+};
+
+const listenTo = (eventName, func) => {
+  _listenTo(eventName, func, eventListeners);
+};
+
+const stopListenTo = (eventName, func) => {
+  _stopListenTo(eventName, func, eventListeners);
+};
+
+const clearEvent = (eventName) => {
+  _clearEvent(eventName, eventListeners);
+};
+
+const triggerGlobal = (eventName, ...args) => {
+  _trigger(eventName, global._nocmsEventListeners, ...args);
+};
+
+const listenToGlobal = (eventName, func) => {
+  _listenTo(eventName, func, global._nocmsEventListeners);
+};
+
+const stopListenToGlobal = (eventName, func) => {
+  _stopListenTo(eventName, func, global._nocmsEventListeners);
+};
+
+const clearEventGlobal = (eventName) => {
+  _clearEvent(eventName, global._nocmsEventListeners);
+};
+
 
 module.exports = {
   listenTo,
+  listenToGlobal,
   stopListenTo,
+  stopListenToGlobal,
   trigger,
+  triggerGlobal,
   clearEvent,
+  clearEventGlobal,
 };
